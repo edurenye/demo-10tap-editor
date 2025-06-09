119f332e221f119d0e42c12899fbe9b22de80a5a
edurenye <edurenye@hotmail.com>
2025-06-09 22:04:16 +0200

    Add 10tap-editor demo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,50 @@
 import { Stack } from 'expo-router';
+import { RichText, Toolbar, useEditorBridge } from '@10play/tentap-editor';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
 
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+const initialContent = `<p>This is a basic example of <b>@10play/tentap-editor</b>!</p>
+<p></p>
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<p></p>
+<p>This is an example of a list:</p>
+<ul>
+  <li>item 1</li>
+  <li>item 2</li>
+  <li>item 3</li>
+</ul>
+<p></p>
+<p>And a checklist:</p>
+<ul data-type="taskList">
+    <li data-type="taskItem" data-checked="true">Done</li>
+    <li data-type="taskItem" data-checked="false">In progress</li>
+    <li data-type="taskItem" data-checked="false">Todo</li>
+</ul>`;
 
 export default function Home() {
+  const editor = useEditorBridge({
+    autofocus: true,
+    avoidIosKeyboard: true,
+    initialContent,
+  });
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
+      <Stack.Screen options={{ title: '10tap Editor' }} />
+      <SafeAreaView style={styles.container}>
+        <RichText editor={editor} />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={styles.keyboardAvoidingView}>
+          <Toolbar editor={editor} />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
     </>
   );
 }
@@ -18,6 +52,10 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+  },
+  keyboardAvoidingView: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 0,
   },
 });
